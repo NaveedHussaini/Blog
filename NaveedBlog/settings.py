b57911a2509f1f8d1d5da53c344119e8ae99a7a9
NaveedHussaini <25naveedhussaini1@gmail.com>
2024-01-24 15:12:51 +0530

    NavBar

--- NaveedBlog/settings.py
+++ NaveedBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x3$c(st4z21_cwnpssz#basf)cyq9rj^en&oig!pd(xd86z%%7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vercel.app','127.0.0.1']
 
 
 # Application definition
